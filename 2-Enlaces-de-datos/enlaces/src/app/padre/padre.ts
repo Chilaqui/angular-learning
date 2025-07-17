e25349693dc37d4ffcb63a9b687036f535cf5e7e
Hector <comhector_@hotmail.com>
2025-07-17 14:26:00 -0600

    actualizando

---
+++ 2-Enlaces-de-datos/enlaces/src/app/padre/padre.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  standalone: false,
+  templateUrl: './padre.html',
+  styleUrl: './padre.css'
+})
+export class Padre {
+
+  valorContador: number = 0;
+
+  incrementarContador(){
+    this.valorContador++;
+  }
+
+  decrementarContador(){
+    this.valorContador--; 
+ }
+
+
+
+}
